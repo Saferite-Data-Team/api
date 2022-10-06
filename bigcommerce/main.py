3c3ea878b0e442407924ee59624ccfd243879d69
estiven199 <esti-ven93@hotmail.com>
2022-10-06 11:01:44 -0500

    client id parameter of the BC class, was removed

--- bigcommerce/main.py
+++ bigcommerce/main.py
@@ -2,8 +2,8 @@ from saferite.core import BCBase
 from bigcommerce.api import *
 
 class BC:
-    def __init__(self, token:str, client:str, store_hash:str):
-        self.customer = customers.Customer(token, client, store_hash)
-        self.order = orders.Order(token, client, store_hash)
-        self.product = products.Product(token, client, store_hash)
-        self.brand = brands.Brand(token, client, store_hash)
+    def __init__(self, token:str, store_hash:str):
+        self.customer = customers.Customer(token, store_hash)
+        self.order = orders.Order(token, store_hash)
+        self.product = products.Product(token, store_hash)
+        self.brand = brands.Brand(token, store_hash)
