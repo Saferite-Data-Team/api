e21cbb4e9b7d21986de2c8631da11ca368d20d6a
estiven199 <esti-ven93@hotmail.com>
2022-11-09 15:56:56 -0500

    added the module Payment"

---
+++ books/api/customer_payment.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from books.data import PaymentData
+
+from saferite.core import ZohoBooksBase, strict_types
+
+@dataclass
+class Payment:
+    token:str
+    organization_id:str
+    
+    def __post_init__(self) -> None:
+        self.base = ZohoBooksBase(self.token, self.organization_id)
+        self.module = "customerpayments"
+        
+    @strict_types
+    def create(self, data:dict):
+        """Create a payment for your invoices
+        Args:
+            data (dict)
+        Returns
+            Response
+        """
+        return self.base.api._standard_call(f'{self.module}', 'post', json_data=data)
+
+
+
+
+
